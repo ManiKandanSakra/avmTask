330e871938c2ff8def3d812f88046575a9dab633
DEll <manisakravarthi@gmail.com>
2023-11-04 21:05:00 +0530

    avmTask completed alt

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -12,8 +12,8 @@ export default class Temperature extends React.Component {
                     <div className='d-flex justify-content-between tempContent'> 
                         <div className='tempeH1'><h1>21 <span className='celcious'>°C</span></h1></div>
                         <div className='temp-innerFont'> 
-                        <span><h5><img src={fire} className='fire-icon'/>26 <span className='content-celcious'>°C</span></h5></span> <hr></hr> 
-                        <h5><img src={snowflake} className='fire-icon'/>18 <span className='content-celcious'>°C</span></h5></div>
+                        <span><h5><img src={fire} className='fire-icon' alt="fire"/>26 <span className='content-celcious'>°C</span></h5></span> <hr></hr> 
+                        <h5><img src={snowflake} className='fire-icon' alt="snowflake"/>18 <span className='content-celcious'>°C</span></h5></div>
                     </div>
                 </Card.Body>  
             </Card> 
