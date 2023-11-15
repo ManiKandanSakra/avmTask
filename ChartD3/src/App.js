e22f839eeb0b656929161628d79005fb92310787
manikandan <manikandan.s@biovustech.com>
2023-11-15 10:14:21 +0530

    charD3

---
+++ ChartD3/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+import './App.css';
+
+import BarChart from './chart/d3chart';
+
+
+const datas = [
+
+    [10, 30, 40, 20],
+
+    [10, 40, 30, 20, 50, 10],
+
+    [60, 30, 40, 20, 30, 20, 70, 60, 90]
+
+]
+
+var i = 0;
+
+
+function App() {
+
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+
+        changeData();
+
+    }, []);
+
+
+    const changeData = () => {
+
+        setData(datas[i++]);
+
+        if(i === datas.length) i = 0;
+
+    }
+
+
+
+    return (
+
+        <div className="App">
+
+            {/* <h2>Graphs with React</h2>
+
+            <button onClick={changeData}>Change Data</button> */}
+
+            <BarChart width={600} height={400} data={data} />
+
+        </div>
+
+    );
+
+}
+
+
+export default App;
